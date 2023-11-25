9123e284103a1f2eae1d6f6f50e6b65b3305ad85
Angel Penaloza <angelpmagallon10@gmail.com>
2023-11-24 23:53:09 -0800

    final update

--- calculator/src/pages/_app.js
+++ calculator/src/pages/_app.js
@@ -1,5 +1,41 @@
 import '@/styles/globals.css'
+import { useState } from 'react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function Num({ value }) {
+  const [output, setNum] = useState(null);
+  
+  function handleNumClick(val) {
+    setNum(val);
+  }
+
+  return ( 
+    <button className="num" onClick={handleNumClick}>
+      {value}
+    </button>
+  );
 }
+
+export default function App() {
+
+  return (
+    <>
+
+      <div class="row">
+        <Num value="1" onNumClick={() => handleNumClick(1)}/>
+        <Num value="2"/>
+        <Num value="3"/>
+      </div>
+      <div class="row">
+        <Num value="4"/>
+        <Num value="5"/>
+        <Num value="6"/>
+      </div>
+      <div class="row">
+        <Num value="7"/>
+        <Num value="8"/>
+        <Num value="9"/>
+      </div>
+    </>
+  );
+}
+
